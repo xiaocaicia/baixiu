476aa512ee261c8f3beccadda61a34dc5335f96b
小菜菜呢 <2540850871@qq.com>
2019-12-08 18:55:54 +0800

    实现轮播图数据的获取

---
+++ public/assets/js/home.js
@@ -0,0 +1,33 @@
+//获取轮播图数据
+// alert(111)
+$.ajax({
+    type:'get',
+    url: '/slides',
+    success:function(res){
+       // console.log(res)
+       var html=template('swipeTpl',{data:res})
+      // console.log(html)
+       $('#slidesBox').html(html);
+        //
+    var swiper = Swipe(document.querySelector('.swipe'), {
+        auto: 2000,
+        transitionEnd: function (index) {
+          // index++;
+  
+          $('.cursor span').eq(index).addClass('active').siblings('.active').removeClass('active');
+        }
+      });
+  
+      // 上/下一张
+      $('.swipe .arrow').on('click', function () {
+        var _this = $(this);
+  
+        if(_this.is('.prev')) {
+          swiper.prev();
+        } else if(_this.is('.next')) {
+          swiper.next();
+        }
+      })
+    }
+    
+})
